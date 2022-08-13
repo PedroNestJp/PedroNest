5a1e8294838c1a1ffc414235a9488114446084c1
Pedro Nest <pedronestjp@gmail.com>
2022-08-12 22:55:17 -0300

    atualização aula-14

---
+++ modulo3/aula-14-VariaveisDadosOperadores/testes.js
@@ -0,0 +1,12 @@
+let frutas = [ 'uva', 'pera', 'maçã', 'salada mista']
+console.log(frutas)
+
+let meusObjetos = {
+    objto1: 1,
+    objto2: 2,
+    objto3: 3,
+    objto4: 4
+}
+
+delete meusObjetos.obejto3
+console.log(meusObjetos)

5dc176ace1df5a348987210da1fe277709c8f6e1
PedroNestJP <pedronestjp@gmail.com>
2022-11-17 01:49:14 -0300

    up orm

--- projetoIntegrador-siteNest/banco/orm/index.js
+++ projetoIntegrador-siteNest/banco/orm/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
 
 app.get ('/', (req, res)=> {
     res.send('hi Pedro')
@@ -17,5 +20,5 @@ app.get ('/usuarios', (req, res)=> {
 })
 
 app.listen (PORT, ()=>{
-    console.log(`✔ serviço rodando na porta: ${PORT}`)
+    console.log(`✔ serviço rodando em: http://localhost:${PORT}`)
 })

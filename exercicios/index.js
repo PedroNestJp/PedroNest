e9376af6b4ed12e2b96584f65f5dfae06d105ed2
PedroNestJP <pedronestjp@gmail.com>
2022-12-05 07:34:33 -0300

    up orm

---
+++ exercicios/index.js
@@ -0,0 +1,20 @@
+let produto = "Maça"
+let quantidadeDoProduto = 0
+let quantidadeFaltandoDoParaPromocao = 5 - quantidadeDoProduto
+let quantidadeRestanteDeMacas = 
+
+function validaFruta(produto){
+if (produto === "Maça") {
+    return `você escolheu a fruta ${produto}`
+}}
+
+function validaPromo(quantidadeDoProduto){
+    if (quantidadeDoProduto >= 5 ){
+        return "Você recebeu uma promoção, o valor do produto sairá por R$5.00"
+    }
+    else (quantidadeDoProduto <=5)  
+        return (`se você adicionar mais ${quantidadeFaltandoDoParaPromocao} o valor sairá por : R$5.00 `)
+     }
+     
+console.log(quantidadeDoProduto(4))
+console.log ()
--- projetoIntegrador-siteNest/banco/orm/index.js
+++ projetoIntegrador-siteNest/banco/orm/index.js
@@ -11,36 +11,91 @@ app.get ('/', (req, res)=> {
     res.send('hi Pedro')
 })
 
+app.post('/produtos', async (req, res)=> {
+    const data = req.body
+    const produtos = await prisma.produtos.create({data})
+    if (produtos){ 
+        res.send("Produto criado com sucesso")
+    }
+})
+
 app.get ('/produtos', async (req, res)=> {
     const produtos = await prisma.produtos.findMany()
-    if (produtos){ res.send(produtos) }
-
+    if (produtos){
+        res.send(produtos)
+    }
 })
 
 app.get ('/produtos/:id', async (req, res)=> {
     const id = parseInt( req.params.id )
-    const produtos = await prisma.produtos.findUnique({where:id})
+    const produtos = await prisma.produtos.findUnique({where:{id}})
     if (produtos){res.send(produtos)}
 
 })
 
-app.get ('/usuarios', async (req, res)=> {
-    const usuarios = await prisma.usuarios.findMany()
-    res.send(usuarios)
+app.delete('/produtos/:id', async (req, res)=> {
+    const id = parseInt(req.params.id)
+    const produtos = await prisma.produtos.delete({where:{id}})
+    if (produtos){
+    res.send("Produto deletado com sucesso ✅")
+    }
+})
+
+
+
+
+app.post('/usuarios', async (req, res)=> {
+    const data = req.body
+    const usuarios = await prisma.usuarios.create({data})
+    res.send('usuário criado com sucesso ✅')
     
 })
 
-app.get ('/produtos/:id', async (req, res)=> {
-    const data = parseInt( req.params.id )
-    const produtos = await prisma.produtos.findUnique({where:id})
-    res.send(produtos)
 
+app.get ('/usuarios', async (req, res)=> {
+    const usuarios = await prisma.usuarios.findMany()
+    if (usuarios){
+    res.send(usuarios)
+    }
+})
+app.get ('/usuarios/:id', async (req, res)=> {
+    const id = parseInt(req.params.id)
+    const usuarios = await prisma.usuarios.findUnique({where:{id}})
+    if (usuarios){
+    res.send(usuarios)
+    }
+})
+app.delete('/usuarios/:id', async (req, res)=> {
+    const id = parseInt(req.params.id)
+    const usuarios = await prisma.usuarios.delete({where:{id}})
+    if (usuarios){
+    res.send("Usuário deletado com sucesso ✅")
+    }
 })
 
-app.get ('/usuariospj', async (req, res)=> {
-    const usuariospj = await prisma.usuariospj.findMany()
-    res.send(usuariospj)
 
+app.post("/usuariospj", async (req,res) => {
+    const data = req.body
+    console.log(data)
+    const usuariospj = await prisma.usuariospj.create({data})
+    if(usuariospj){
+        res.send('UsuárioPJ criado com sucesso ✅')
+    }
+})
+
+app.get('/usuariospj', async (req,res)=>{
+    const usuariospj = await prisma.usuariospj.findMany()
+    if (usuariospj) {
+        res.send(usuariospj)
+    }
+})
+
+app.get('/usuariospj/:id', async (req,res)=>{
+    const id = parseInt(req.params.id)
+    const usuariospj = await prisma.usuariospj.findUnique({where:{id}})
+    if (usuariospj){
+        res.status(201).send(` O usuárioPJ ${usuariospj.nomeFantasia} de cnpj:${usuariospj.cnpj} cujo id:${id}, foi encontrado ✅`)
+    }
 })
 
 app.listen (PORT, ()=>{

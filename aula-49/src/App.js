e715b13d640831f661e7decf7de82486a4679426
PedroNestJP <pedronestjp@gmail.com>
2022-12-17 13:47:48 -0300

    add react-statrless/ful

---
+++ aula-49/src/App.js
@@ -0,0 +1,14 @@
+
+import StateFul from './StateFul';
+import Sobre from './Sobre'
+
+function App() {
+  return (
+    <>
+    <StateFul/>
+    <Sobre></Sobre>
+    </>
+  );
+}
+
+export default App;

1addc8ddf56cfb2d8390390e0e1326fa763834a5
PedroNestJP <pedronestjp@gmail.com>
2022-12-22 09:27:46 -0300

    add folder aula-51

--- aula-51-1/src/App.js
+++ aula-51-1/src/App.js
@@ -1,28 +1,27 @@
-import {BrowserRouter, Route, Router } from 'react-router-dom'
+import {BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home';
 import Produtos from './pages/Produtos';
 import Sobre from './pages/Sobre';
 import './App.css';
-import Nav from './components/Nav'; 
-
+import Nav from './components/Nav'
 
 function App() {
   return (
     <>
-    
       <BrowserRouter>
-      <Nav/>
-        <Router>
+      <Nav></Nav>
+        <Routes>
           <Route path="/" index element={ <Home/> } />
           <Route path="/produtos"  element={<Produtos/>} />
           <Route path="/sobre"  element={<Sobre/>} />
-        </Router>
+        </Routes>   
       </BrowserRouter>
-      <footer>
-        <h3>  por Pedro Nest </h3>
-      </footer>
+   
+        <footer>
+          <h3>  por Pedro Nest </h3>
+        </footer>
     </>
-  )
+  );
 }
 
 export default App;
